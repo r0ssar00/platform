37896482d965b45d89f2872f079b50630ceb7d51
Kevin Ross <r0ssar00@gmail.com>
2009-07-28 13:32:13 -0400

    delete everything to start fresh

--- SettingsKeys.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-// header contains all of the settings keys used by the suite
-const char * AliasTypeKey = "com.ross.platform.AliasType";
-const char * CountCheckKey = "com.ross.XTagRev.Time.CountCheck";
-const char * FullRefreshKey = "com.ross.XTagRev.Time.FullRefresh";
-const char * ThrottleTimeKey = "com.ross.XTagRev.Time.ThrottePeriod";
-const char * TagFolderKey = "com.ross.XTagRev.TagFolder";
-const char * TagDBTag = "XTagRevDatabaseFile";
